75a53b7ee160e77236245d415fab6aada55892c9
ktak <ktak.dev@gmail.com>
2022-05-16 14:59:01 +0900

    feat(index): add logo animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,25 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import lottie from 'lottie-web'
+import reactLogo from '../logo.json'
+import { useEffect, useRef } from 'react'
 
 const Home: NextPage = () => {
+  // react 18 work around.
+  // without this, useEffect is called twice in dev mode.
+  const mounted = useRef(false)
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = true
+      lottie.loadAnimation({
+        container: document.getElementById(styles.logo) as Element,
+        animationData: reactLogo,
+        loop: false,
+      })
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,44 +30,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <div id={styles.logo} style={{ width: '400px' }}></div>
       </main>
 
       <footer className={styles.footer}>
